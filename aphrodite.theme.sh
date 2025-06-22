a147fc6b8bd226ff40b9658f40121b05fd3ea850
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-06-22 19:28:40 +0800

    更新 bash 版本以避免输出多余的空格

--- aphrodite.theme.sh
+++ aphrodite.theme.sh
@@ -26,7 +26,7 @@ __aphrodite_update_prompt_data() {
 
 	# I don't need git status output, i'm already using IDE or $(git status).
 	# If i want take some mistake, those prompt con't stopping me.
-	# 
+	#
 	# __aphrodite_git=''
 	# __aphrodite_git_color=$(tput setaf 10)  # clean
 	# local git_branch=$(git --no-optional-locks branch --show-current 2> /dev/null)
@@ -40,21 +40,20 @@ __aphrodite_update_prompt_data() {
 	[[ "$RETVAL" -ne 0 ]] && __aphrodite_prompt_symbol_color=$(tput setaf 1)
 
 	__aphrodite_prompt_return_string=""
-	[[ "$RETVAL" -ne 0 ]] && __aphrodite_prompt_return_string="[${RETVAL}]"
-
+	[[ "$RETVAL" -ne 0 ]] && __aphrodite_prompt_return_string=" [${RETVAL}]"
 
 	return $RETVAL  # to preserve retcode
 }
 
 
 # I don't know what those did, is anyone seted "git_branch" befor bashrc?
-# 
+#
 # if [[ -n "$git_branch" ]]; then
 # 	PROMPT_COMMAND="$PROMPT_COMMAND; __aphrodite_update_prompt_data"
 # else
 # 	PROMPT_COMMAND="__aphrodite_update_prompt_data"
 # fi
-# 
+#
 # But we still need PROMP_COMMAND, right? i forget this.
 PROMPT_COMMAND="__aphrodite_update_prompt_data"
 
@@ -63,24 +62,24 @@ PROMPT_COMMAND="__aphrodite_update_prompt_data"
 # APHRODITE_THEME_SHOW_TIME='1'
 
 PS1=''
-PS1+='\[$(tput setaf 7)\]$(echo -ne $__aphrodite_venv)\[$(tput sgr0)\]'
+PS1+='\[$(tput setaf 7)\]$(echo -ne "$__aphrodite_venv")\[$(tput sgr0)\]'
 PS1+='\[$(tput setaf 6)\]\u'
 PS1+='\[$(tput setaf 8)\]@'
 PS1+='\[$(tput setaf 12)\]\h'
 # Add a space after ':' for easy copying, i love double click.
-PS1+='\[$(tput setaf 8)\]: '
-PS1+='\[$(tput sgr0)\]\w '
+PS1+='\[$(tput setaf 8)\]:'
+PS1+=' \[$(tput sgr0)\]\w'
 
 # git state
-# PS1+='\[$(echo -ne $__aphrodite_git_color)\]$(echo -ne $__aphrodite_git)\[$(tput sgr0)\] '
+# PS1+=' \[$(echo -ne "$__aphrodite_git_color")\]$(echo -ne "$__aphrodite_git")\[$(tput sgr0)\]'
 # PS1+='\[$(tput sgr0)\]'
 
 # time
-PS1+='\[$(tput setaf 8)\]\[$([[ -n "$APHRODITE_THEME_SHOW_TIME" ]] && echo -n "[\t]")\]\[$(tput sgr0)\] '
+PS1+='\[$(tput setaf 8)\]\[$([[ -n "$APHRODITE_THEME_SHOW_TIME" ]] && echo -n " [\t]")\]\[$(tput sgr0)\]'
 
 # error code
-PS1+='\[$(echo -ne $__aphrodite_prompt_symbol_color)\]'
+PS1+='\[$(echo -ne "$__aphrodite_prompt_symbol_color")\]'
 # display error code and line feed
-PS1+='\[$(echo -ne $__aphrodite_prompt_return_string)\]\n'
+PS1+='\[$(echo -ne "$__aphrodite_prompt_return_string")\]\n'
 # set to normal color
 PS1+='\[$(tput sgr0)\]\$ '
